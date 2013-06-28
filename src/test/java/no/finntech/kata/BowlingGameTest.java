5d2dc60249433c2c2b8e4c2367c71c05e7d1d13b
fihegjet <henning.gjetanger@finntech.no>
2013-06-28 13:27:02 +0200

    More cleanup

--- src/test/java/no/finntech/kata/BowlingGameTest.java
+++ src/test/java/no/finntech/kata/BowlingGameTest.java
@@ -9,21 +9,31 @@ public class BowlingGameTest {
 
     @Test
     public void shouldGetScore_1_when_1_0() {
-        assertThat(BowlingGame.getPlayer1Score(new BowlingGame.Frame(1, 0))).isEqualTo(1);
+        assertThat(BowlingGame.getPlayer1Score(
+                new BowlingGame.Frame(1, 0)))
+                .isEqualTo(1);
     }
 
     @Test
     public void shouldGetScore_2_when_1_1() {
-        assertThat(BowlingGame.getPlayer1Score(new BowlingGame.Frame(1, 1))).isEqualTo(2);
+        assertThat(BowlingGame.getPlayer1Score(
+                new BowlingGame.Frame(1, 1)))
+                .isEqualTo(2);
     }
 
     @Test
     public void spareShouldAddNextThrowToFrameScore() {
-        assertThat(BowlingGame.getPlayer1Score(new BowlingGame.Frame(1, 9), new BowlingGame.Frame(8, 0))).isEqualTo(26);
+        assertThat(BowlingGame.getPlayer1Score(
+                new BowlingGame.Frame(1, 9),
+                new BowlingGame.Frame(8, 0)))
+                .isEqualTo(26);
     }
 
     @Test
     public void strikeShouldAddNextTwoThrowsToFrameScore() {
-        assertThat(BowlingGame.getPlayer1Score(new BowlingGame.Frame(10, 0), new BowlingGame.Frame(1, 2))).isEqualTo(16);
+        assertThat(BowlingGame.getPlayer1Score(
+                new BowlingGame.Frame(10, 0),
+                new BowlingGame.Frame(1, 2)))
+                .isEqualTo(16);
     }
 }
